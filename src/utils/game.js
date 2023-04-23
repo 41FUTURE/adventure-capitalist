253dd9c0f905c893709951e0a84278b1aef6181a
Ayhem <99112509+41FUTURE@users.noreply.github.com>
2023-04-23 21:02:17 +0900

    Update game.js

--- src/utils/game.js
+++ src/utils/game.js
@@ -1,18 +1,22 @@
-import { getCloseTime } from '../localStorage'; // Import the getCloseTime function from the localStorage module
-import { millisecondsToStr } from './time'; // Import the millisecondsToStr function from the time module
+// Import the getCloseTime function from the localStorage module
+import { getCloseTime } from '../localStorage';
+
+// Import the millisecondsToStr function from the time module
+import { millisecondsToStr } from './time';
 
 // This function processes the background calculations for the game state
 export const processBackgroundCalculating = state => {
-  let earning = 0;
-  const now = (new Date()).getTime();
+  let earning = 0; // Initialize earning variable to 0
+  const now = (new Date()).getTime(); // Get current time
   const closeTime = getCloseTime() || now; // Get the time when the window was last closed or use the current time if it hasn't been closed before
 
+  // Loop through all businesses in the state object
   Object.values(state.businesses).forEach(item => {
     // Calculate earnings for auto-running businesses
     if (item.quantityPurchased && item.hasManager && item.lastRun) {
       const elapsedSeconds = Math.floor((now - item.lastRun)/1000);
       const completedTimes = Math.floor(elapsedSeconds/item.timeTaken);
-      earning += completedTimes * item.profit;
+      earning += completedTimes * item.profit; // Add to earning the profit made by the business in the elapsed time
       // Set new lastRun
       item.lastRun += completedTimes*item.timeTaken*1000;
     }
@@ -25,21 +29,25 @@ export const processBackgroundCalculating = state => {
     if (item.quantityPurchased && !item.hasManager && item.lastRun 
       && (closeTime - item.lastRun) < item.timeTaken * 1000)  {
       if ((now - item.lastRun) >= item.timeTaken * 1000) {
-        earning += item.profit;
+        earning += item.profit; // Add to earning the profit made by the business
       }
     }
-    state.businesses[item.id] = item;
+    state.businesses[item.id] = item; // Update the state with the updated business object
   });
   
-  state.balance.amount += earning;
+  state.balance.amount += earning; // Add the total earnings to the balance of the state object
   
   // Set the awayEarning property to show how much money was earned while the window was closed and how long it was closed for
   state.awayEarning = {
-    amount: earning,
-    awayDuration: millisecondsToStr(now - closeTime)
+    amount: earning, // Set the amount of money earned while the window was closed
+    awayDuration: millisecondsToStr(now - closeTime) // Set the duration the window was closed for
   };
-  return state;
+  return state; // Return the updated state object
 }
 
 // This function converts an object to a sorted list based on the order property of each object
-export const objectToList = object => Object.values(object).sort((a, b) =>  a.order - b.order);
+export const objectToList = object => {
+  // Convert the object to an array using Object.values()
+  // Sort the array based on the order property of each object
+  return Object.values(object).sort((a, b) =>  a.order - b.order);
+}
